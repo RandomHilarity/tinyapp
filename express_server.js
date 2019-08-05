acd39f9a801e4b1fa3f7be64189ac75cf40b3eea
John Webster <johnrwebs@gmail.com>
2019-08-05 23:24:27 +0000

    added URL addition page, links in express_server and random string generator function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
+const generateRandomString = function() {
+  return Math.random().toString(36).split('').filter(function(value, index, self) {
+    return self.indexOf(value) === index;
+  }).join('').substr(2,6);
+};
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": 'http://www.lighthouselabs.ca',
@@ -14,13 +22,23 @@ app.get('/', (request, response) => {
   response.send('Hello!');
 });
 
+app.post('urls', (request, response) => {
+  console.log(request.body);
+  response.send('Ok');
+});
+
 app.get('/urls', (request, response) => {
   let templateVars = { urls: urlDatabase };
   response.render('urls_index', templateVars);
 });
 
+
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
+});
+
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (request, response) => {
