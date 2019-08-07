97070acb586e135a761c2f225bce7db21b94876e
John Webster <johnrwebs@gmail.com>
2019-08-07 23:41:16 +0000

    added bcrypt, additional explanations

--- express_server.js
+++ express_server.js
@@ -1,21 +1,13 @@
+//CONSTANTS W/ REQUIRES
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const bcrypt = require('bcrypt');
 const PORT = 8080;
 
-/* const isLoggedIn = function() {
-  app.get(user_id, (response, request) => {
-    if (!user_id) {
-      let templateVars = {
-        user: users[request.cookies["user_id"]]
-      };
-      response.render('login', templateVars);
-    }
-    response.render('login');
-  });
-}; */
-
+//GLOBAL FUNCTIONS
 
 const generateRandomString = function() {
   return Math.random().toString(36).split('').filter(function(value, index, self) {
@@ -42,28 +34,34 @@ const urlIsForUser = function(id) {
   return userUrlObj;
 };
 
+//SET USE
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-//app.use(isLoggedIn());
+
+//TESTING ACCOUNTS AND DATA OBJECTS
 
 let urlDatabase = {
-  "b2xVn2": { longurl: 'http://www.lighthouselabs.ca', userID: "userRandomID" },
-  "9sm5xK": { longurl: 'http://www.google.com', userID: "user2RandomID" }
+  "b2xVn2": { longurl: 'http://www.lighthouselabs.ca', userID: "7bwcl5" },
+  "9sm5xK": { longurl: 'http://www.google.com', userID: "gqe7hr" }
 };
 
+//unhashed PW for both test accounts is password
 let users = {
-  "userRandomID": {
-    id: "userRandomID",
+  "7bwcl5": {
+    id: "7bwcl5",
     email: "1@1.com",
-    password: "password"
+    password: "$2b$10$t0CtHP4/uEVkDyF1wktgYe.IZPFhpTp60gxlOAcgy6leVt6W6rAe6"
   },
-  "user2RandomID": {
-    id: "user2RandomID",
+  "gqe7hr": {
+    id: "gqe7hr",
     email: "2@2.com",
-    password: "password"
+    password: "$2b$10$YOI.Y10.NFs1Pd6aXpcv1Oq11Xl/12PTnVQPU.Lw9/Fa.iG0A.dvC"
   },
 };
+
+//GET AND POST
 
 //ROOT path handler
 app.get('/', (request, response) => {
@@ -97,6 +95,7 @@ app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
 });
 
+// GOTO new_URLs page, goes to login if user not logged in, only user URLs
 app.get('/urls/new', (request, response) => {
   let id = request.cookies["user_id"];
   if (!users[request.cookies["user_id"]]) {
@@ -112,12 +111,12 @@ app.get('/urls/new', (request, response) => {
   response.render('urls_new', templateVars);
 });
 
+// GOTO shortURL page if owned by user, error if not owned/logged in
 app.get('/urls/:shortURL', (request, response) => {
   let id = request.cookies["user_id"];
   let reqUrl = request.params.shortURL;
   if (id !== urlDatabase[reqUrl].userID) {
     response.status(403).send("shortURL not owned by user");
-    return;
   }
   let templateVars = {
     shortURL: reqUrl,
@@ -127,7 +126,7 @@ app.get('/urls/:shortURL', (request, response) => {
   response.render('urls_show', templateVars);
 });
 
-// DELETE - deletes short/long URL entry from urlDatabase object
+// DELETE - deletes short/long URL entry from urlDatabase object, check if logged in/owner
 app.post('/urls/:shortURL/delete', (request, response) => {
   let id = request.cookies["user_id"];
   let reqUrl = request.params.shortURL;
@@ -139,6 +138,7 @@ app.post('/urls/:shortURL/delete', (request, response) => {
   response.redirect('/urls');
 });
 
+// GOTO shortURL link on web, universally accessible
 app.get('/u/:shortURL', (request, response) => {
   let reqUrl = request.params.shortURL;
   const longURL = urlDatabase[reqUrl].longurl;
@@ -165,11 +165,11 @@ app.post('/login', (request, response) => {
     response.status(403).send('Email does not exist, register or re-enter');
   }
   for (let userID in users) {
-    if (uEmail === users[`${userID}`]["email"] && uPass !== users[`${userID}`]["password"]) {
+    let pwMatch = bcrypt.compareSync(uPass, users[`${userID}`]["password"]);
+    if (uEmail === users[`${userID}`]["email"] && !pwMatch) {
       response.status(403).send("Incorrect password");
-      return;
     }
-    if (uEmail === users[`${userID}`]["email"] && uPass === users[`${userID}`]["password"]) {
+    if (uEmail === users[`${userID}`]["email"] && pwMatch) {
       response.cookie('user_id', userID);
       return response.redirect('urls/');
     }
@@ -195,11 +195,11 @@ app.post('/register', (request, response) => {
     response.status(400).send('Email already exists.');
     return;
   }
-  
   let newID = generateRandomString();
+  let hashPW = bcrypt.hashSync(request.body["password"], 10);
   users[newID] = { id: newID,
     email: request.body["email"],
-    password: request.body["password"]
+    password: hashPW
   };
   response.cookie('user_id', newID);
   return response.redirect('urls/');
